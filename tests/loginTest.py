ce388b77e2210f48cebe689fd2ad20b0f8279998
Nishikant Singh <34189127+bratva123@users.noreply.github.com>
2020-06-15 12:42:29 +0530

    Update loginTest.py

--- tests/loginTest.py
+++ tests/loginTest.py
@@ -8,11 +8,31 @@ class LoginTests(unittest.TestCase):
 
     email = "9931145329"
     password = "nishi@1234"
+    _keyword = "Headphone"
+    _wrongKeyword = "lkjhjhbs;fb;jsdfbdf"
 
     @pytest.fixture(autouse=True)
     def classSetup(self, oneTimeSetUp):
+        self.driver = oneTimeSetUp
         self.lp = LoginPage(self.driver)
+        self.sp = SearchPage(self.driver)       
 
+
+    @pytest.mark.run(order=5)
+    def test_validKeyword(self):
+        self.sp.search(self._keyword)
+        time.sleep(3)
+        result = self.sp.isThereAnyResult()
+        assert result == False
+
+    @pytest.mark.run(order=4)
+    def test_invalidKeyword(self):
+        self.driver.find_element_by_xpath("//button[@class='_2AkmmA _29YdH8']").click()
+        self.sp.search(self._wrongKeyword)
+        time.sleep(3)
+        result = self.sp.isThereAnyResult()
+        assert result == True
+        
     @pytest.mark.run(order=3)
     def test_validLogin(self):
         self.lp.login(self.email, self.password)
